b2ae5d8980634f7c3f08926b0da99e6e4e3a772c
Aaron Jensen <aaronjensen@gmail.com>
2018-02-02 13:11:09 -0800

    Use jest so I can use babel

---
+++ test/Multicast.test.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import {render} from 'react-dom'
+import {MulticastProvider} from '../src'
+
+describe('Mulitcast', () => {
+  it('', () => {
+    render(<MulticastProvider></MulticastProvider>, document.createElement('x-root'))
+  })
+})
